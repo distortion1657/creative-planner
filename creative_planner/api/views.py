0bebb160cd8ede396a1a935d588b49248bf96aae
distortion1657 <agamanstha41@gmail.com>
2026-02-01 11:45:06 -0500

    feat: Base App

---
+++ creative_planner/api/views.py
@@ -0,0 +1,10 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(["GET"])
+def get_tasks(request):
+    example_task = {
+        'name': 'Build this app!',
+        'urgent': True
+    }
+    return Response(example_task)
