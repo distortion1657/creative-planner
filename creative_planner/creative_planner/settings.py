8d445f556692ad735f6bf62ae11edc5a46398da8
distortion1657 <agamanstha41@gmail.com>
2026-02-03 21:46:16 -0500

    feat: Pointed auth_user_model to CustomUser

--- creative_planner/creative_planner/settings.py
+++ creative_planner/creative_planner/settings.py
@@ -77,11 +77,13 @@ WSGI_APPLICATION = 'creative_planner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS':{
+            'service': 'supabase'
+        }
     }
 }
-
+AUTH_USER_MODEL = "base.CustomUser"
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
