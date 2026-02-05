1b711f9ec9b1e72ac2aa41045d50b63c8fe9269e
distortion1657 <agamanstha41@gmail.com>
2026-02-04 23:09:01 -0500

    feat: Added api endpoints for fetching and creating users

--- creative_planner/api/urls.py
+++ creative_planner/api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_tasks)
+    path('', views.get_tasks),
+    path('users/<username>/', views.get_user, name="user"),
+    path('users/', views.create_user, name="create_user"),
 ]
--- creative_planner/api/views.py
+++ creative_planner/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from django.http import Http404
+from django.db import IntegrityError
+from base.models import CustomUser
+from base.helpers.validators import validateString, validateEmail
 @api_view(["GET"])
 def get_tasks(request):
     example_task = {
@@ -9,3 +12,29 @@ def get_tasks(request):
     }
     return Response(example_task)
 
+@api_view(["GET"])
+def get_user(request, username):
+    try:
+        usrname=CustomUser.objects.get(username=username)
+        user = {
+            'username': usrname.username,
+            'email': usrname.email
+        }
+        return Response(user)
+    except CustomUser.DoesNotExist:
+        raise Http404("User does not exist :(")
+    
+@api_view(["POST"])
+def create_user(request):
+    validateString(request.data.get("username", request.data.get("password")))
+    validateEmail(request.data.get("email"))
+    try:
+        user = CustomUser.objects.create(
+            username=request.data.get("username"),
+            password=request.data.get("password"), # Password doesn't get hashed.
+            email=request.data.get("email")
+        )
+    except IntegrityError:
+        return Response("User already exists? Email already exists?")
+    return Response(f"User {user.username} created successfully ")
+    
