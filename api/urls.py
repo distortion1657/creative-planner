86670a82a424404fbeeb296d2365155e92cdd143
distortion1657 <agamanstha41@gmail.com>
2026-02-06 22:49:06 -0500

    feat: Added endpoints for ProductiveObject class, also added a beta version of productivity gain mechanism

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.get_tasks),
     path('users/<username>/', views.get_user, name="user"),
-    path('users/', views.create_user, name="create_user"),
+    path('users/register', views.create_user, name="create_user"),
+    path('productivity/gain', views.productivity_gain, name="productivity_gain"),
+    path('productivity/create', views.create_productive_object, name = "create_productivity_object")
 ]
--- api/views.py
+++ api/views.py
@@ -2,8 +2,10 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import Http404
 from django.db import IntegrityError
-from base.models import CustomUser
-from base.helpers.validators import validateString, validateEmail
+from django.contrib.auth.hashers import make_password
+from base.models import CustomUser, ProductiveObject
+from base.helpers.validators import validateString, validateEmail, validateUser
+
 @api_view(["GET"])
 def get_tasks(request):
     example_task = {
@@ -31,10 +33,54 @@ def create_user(request):
     try:
         user = CustomUser.objects.create(
             username=request.data.get("username"),
-            password=request.data.get("password"), # Password doesn't get hashed.
+            password=make_password(request.data.get("password")), # Password doesn't get hashed.
             email=request.data.get("email")
         )
     except IntegrityError:
         return Response("User already exists? Email already exists?")
     return Response(f"User {user.username} created successfully ")
     
+@api_view(["POST"])
+def productivity_gain(request):
+    try:
+        user = CustomUser.objects.get(username=request.data.get("username"))
+        user_freetime = user.freetime.total_seconds()
+        task = ProductiveObject.objects.get(user=user)
+        task_duration = task.duration.total_seconds()
+        productivity_gained = (task_duration/user_freetime)
+        user.productivity = productivity_gained
+    except CustomUser.DoesNotExist:
+        raise Http404("User does not exist :(")
+    return Response(f"${user.username} gained ${productivity_gained}.")
+
+# For now, we are fetching using the id, but maybe we would need to fetch using the name?
+@api_view(["GET"])
+def get_productive_object(request, id):
+    try:
+        productive_object = ProductiveObject.objects.get(id=id)
+    except ProductiveObject.DoesNotExist:
+        raise Http404("Object does not exist :(")
+
+@api_view(["POST"])
+def create_productive_object(request):
+    parent = validateUser(request.data.get("user"))
+    productive_object = ProductiveObject.objects.create(
+        user=parent,
+        name=request.data.get("name"),
+        productivity_value=request.data.get("productivity_value"),
+        duration=request.data.get("duration")
+    )
+    return Response(f"${productive_object} was successfully created.")
+
+# To Do
+# Write api endpoints for ProductiveObject (GET and POST)
+# Write a getter for CustomUser
+
+# curl -X POST http://localhost:8000/productivity/gain \
+#   -H "Content-Type: application/json" \
+#   -d '{
+#     "user": 6,
+#     "name": "Deep Work Session",
+#     "productivity_value": 82.5,
+#     "duration": "PT1H15M"
+#   }'
