1d994a7932a210a8b758bfcd1256b6d01c0c9653
distortion1657 <agamanstha41@gmail.com>
2026-02-06 22:53:34 -0500

    feat/fix: Added new fields for classes CustomUser and ProductiveObject

--- base/models.py
+++ base/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+import uuid
 from datetime import timedelta
 # Create your models here.
 
@@ -10,14 +10,22 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True, blank=False, null=False)
     creation = models.DateTimeField(auto_now_add=True)
     premium = models.BooleanField(default=False)
-
+    #I'm unsure if this should be durationField or timeField
+    freetime = models.DurationField(default=timedelta())
+    productivity = models.FloatField(default=0.0)
     REQUIRED_FIELDS = ['email']
 
     def __str__(self):
         return f"${self.username}"
 
 class ProductiveObject(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE) # Cascade means if the parent gets deleted, automatically delete everything that depends on it
     name = models.CharField(max_length=255)
+    public_id = models.UUIDField(
+        default=uuid.uuid4(), # uuid4 generates a uuid completely randomly, hence is used here
+        primary_key=True,
+        editable=False,
+        unique=True)
     productivity_value = models.FloatField(
         validators=[MinValueValidator(0.0), MaxValueValidator(100.0)]
     )
